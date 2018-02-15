4095853800cff8df98f5afea2d7cedd195579c1b
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-15 09:32:40 +0000

    Amended build process - Version 7.2.7

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,11 @@
 NAME="${1}"
 version=$(./scripts/bash/get-site-version.sh)
 
-docker build -t $NAME:$version .
+docker build -t $NAME:interim .
+docker run --name $NAME -d $NAME:interim
+sleep 20
+docker exec $NAME ./start.sh
+docker commit $NAME $NAME:$verson
+docker stop $NAME
+docker rm $NAME
+docker rmi $NAME:interim --force
